651087e94d9b4b8af1d638cc32ec979b25e56f21
Arne Gevaert <arnegevaert@gmail.com>
2017-08-24 17:03:23 +0200

    Created class and added test file

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const Observable = require('rxjs/Observable');
+const ldfetch = require('ldfetch');
+const moment = require('moment');
+const lodash = require('lodash');
 
 const catalogUrl = 'https://datapiloten.be/parking/catalog.ttl';
 
-console.log('Hello world');
+class SmartflandersDataQuery {
+  constructor(catalog) {
+    this.catalog = catalog;
+    this.fetch = new ldfetch();
+  }
+}
+
+module.exports = SmartflandersDataQuery;
---
+++ test.js
@@ -0,0 +1,3 @@
+const query = require('./index.js');
+
+let dq = new query('https://datapiloten.be/parking/catalog.ttl');
