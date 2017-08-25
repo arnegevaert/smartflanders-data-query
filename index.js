d02b539da38fddbbdf62a80ebd5a91f824b7cf8c
Arne Gevaert <arnegevaert@gmail.com>
2017-08-25 10:20:39 +0200

    Get all parkings using observable

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const Observable = require('rxjs/Observable');
+const Rx = require('rx');
 const ldfetch = require('ldfetch');
 const moment = require('moment');
 const lodash = require('lodash');
+const n3 = require('n3');
 
 class SmartflandersDataQuery {
-  constructor(catalog) {
+  constructor() {
     this.fetch = new ldfetch();
-    this.catalog = [];
+    this._catalog = [];
     this.buildingBlocks = {
       oDcatDataset: 'http://www.w3.org/ns/dcat#Dataset',
+      oDatexUrbanParkingSite: 'http://vocab.datex.org/terms#UrbanParkingSite',
+      pDatexParkingNumberOfSpaces: 'http://vocab.datex.org/terms#parkingNumberOfSpaces',
+      pRdfsLabel: 'http://www.w3.org/2000/01/rdf-schema#label',
       pDcatDistribution: 'http://www.w3.org/ns/dcat#distribution',
       pDcatDownloadUrl: 'http://www.w3.org/ns/dcat#downloadURL',
       pRdfType: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
@@ -45,13 +49,22 @@ class SmartflandersDataQuery {
 
       // Add links to catalog
       downlinks.forEach(dl => {
-        if (this.catalog.indexOf(dl.object) === -1) {
-          this.catalog.push(dl.object);
+        if (this._catalog.indexOf(dl.object) === -1) {
+          this._catalog.push(dl.object);
         }
       });
     });
   }
 
+  // Adds a simple dataset URL to the internal catalog
+  addDataset(dataset) {
+    if (this._catalog.indexOf(dataset) === -1) {
+      this._catalog.push(dataset);
+    }
+  }
+
+  // Filters a list of triples using a given template
+  // Returns an array of matching triples
   filterTriples(template, triples) {
     let result = [];
     triples.forEach(t => {
@@ -65,6 +78,53 @@ class SmartflandersDataQuery {
     });
     return result;
   }
+
+  // Gets parkings from all datasets in catalog
+  // Returns an Observable
+  getParkings() {
+    return Rx.Observable.create(observer => {
+      this._catalog.forEach(datasetUrl => {
+        this.fetch.get(datasetUrl).then(response => {
+          // Get all subjects that are parkings
+          const parkings = [], totalspacesParking = [], labels = [];
+          response.triples.forEach(triple => {
+            if (triple.object === this.buildingBlocks.oDatexUrbanParkingSite) {
+              parkings.push(triple);
+            }
+            if (triple.predicate === this.buildingBlocks.pDatexParkingNumberOfSpaces) {
+              totalspacesParking.push(triple);
+            }
+            if (triple.predicate === this.buildingBlocks.pRdfsLabel) {
+              labels.push(triple);
+            }
+          });
+          if (parkings.length <= 0) {
+            observer.onError('No parkings found in dataset: ', datasetUrl);
+          }
+          parkings.forEach(parking => {
+            const totalspacesresult = lodash.find(totalspacesParking, (o) => o.subject === parking.subject);
+            const totalspaces = parseInt(n3.Util.getLiteralValue(totalspacesresult.object), 10);
+            const labelresult = lodash.find(labels, (o) => {
+              return o.subject === parking.subject
+            });
+            const rdfslabel = n3.Util.getLiteralValue(labelresult.object);
+            const id = rdfslabel.replace(' ', '-').toLowerCase();
+            const parkingObj = {
+              label: rdfslabel,
+              uri: parking.subject,
+              id: id,
+              totalSpaces: totalspaces,
+              datasetUrl: datasetUrl,
+            }
+            observer.onNext(parkingObj);
+          })
+          observer.onComplete();
+        });
+      });
+    });
+  }
+
+  getCatalog() { return this._catalog; }
 }
 
 module.exports = SmartflandersDataQuery;
--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const query = require('./index.js');
 
 let dq = new query();
-dq.addCatalog('https://datapiloten.be/parking/catalog.ttl').then(() => {
-  console.log(dq.catalog);
-});
+dq.addCatalog('https://datapiloten.be/parking/catalog.ttl')
+  .then(() => {
+    console.log(dq.getCatalog());
+    dq.getParkings().subscribe(parking => {
+      console.log(parking.label, parking.uri);
+    },() => {}, () => {});
+  });
